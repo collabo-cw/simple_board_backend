7164afdc0a6bedaa6d06281078b29e02298e5341
Min Leo <mck0819@gmail.com>
2024-09-11 15:24:03 +0900

    240911 어드민 코드리뷰 테스트

--- board/admin.py
+++ board/admin.py
@@ -6,4 +6,7 @@ from board.models import Board
 # Register your models here.
 @admin.register(Board)
 class BoardAdmin(admin.ModelAdmin):
-    pass
+    model = Board
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    search_fields = ('title', 'author', 'created_at', 'updated_at')
+    list_filter = ('title', 'author', 'created_at', 'updated_at')
